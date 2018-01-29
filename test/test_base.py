f973324ac93fc7b72e6f2c5c62384e7a20456420
Ian Wright <tech@wrighting.org>
2018-01-29 19:59:46 +0000

    Run tests against AWS

--- test/test_base.py
+++ test/test_base.py
@@ -5,12 +5,15 @@ import json
 import requests
 import os
 
+import boto.cloudformation
+
 class TestBase(unittest.TestCase):
 
 
     _auth_token = None
     _configuration = None
     _api_client = None
+    _region = None
 
     """
     """
@@ -21,10 +24,25 @@ class TestBase(unittest.TestCase):
             at = r.text.split('=')
             token = at[1].split('&')[0]
             self._auth_token = token
+            self._region = args['region']
+
         self._configuration = swagger_client.Configuration()
         self._configuration.access_token = self._auth_token
-        self._api_client = swagger_client.ApiClient(self._configuration)
 
+        conn = boto.cloudformation.connect_to_region(self._region)  # or your favorite region
+        stacks = conn.describe_stacks('wrighting-example-service-dev')
+        if len(stacks) == 1:
+            stack = stacks[0]
+            for output in stack.outputs:
+                if output.key == 'ServiceEndpoint':
+                    self._configuration.host=output.value + '/example-service/v1'
+
+#                print('%s=%s (%s)' % (output.key, output.value, output.description))
+        else:
+                # Raise an exception or something because your stack isn't there
+                pass
+
+        self._api_client = swagger_client.ApiClient(self._configuration)
 
     """
     """
